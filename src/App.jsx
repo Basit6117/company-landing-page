1b12127bf729e6fa19ceff9f6f78067e851ba617
Abdul Basit <abdul.b6117@gmail.com>
2025-09-30 15:31:25 +0500

    company landing page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import Button from "./components/UI/Button"
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+function App() {
+
+  return (
+<>
+<Navbar />
+<Footer />
+</>
+  )
+}
+
+export default App
